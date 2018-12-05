8605e186ddef485214ec19c4520403c99b208708
Yordi De Rijcke <yordi.derijcke@student.hogent.be>
2018-12-05 02:30:21 +0100

    Add project files.

---
+++ T3.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace T3.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
