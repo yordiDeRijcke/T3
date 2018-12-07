e3c321b53294f0cd8b93290c7a88486c490c52f1
yordiDeRijcke <yordi.derijcke@student.hogent.be>
2018-12-07 21:14:34 +0100

    Add Views & Controller for Bill

---
+++ T3.Web/Controllers/BillController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using T3.Core.Repositories;
+
+namespace T3.Web.Controllers
+{
+    [Authorize]
+    public class BillController : Controller
+    {
+        #region Properties
+        private readonly IBillRepository _billRepository;
+        #endregion
+
+        #region Constructor
+        public BillController(IBillRepository billRepository)
+        {
+            _billRepository = billRepository;
+        }
+        #endregion
+
+        public IActionResult Index()
+        {
+            return View(_billRepository.GetAllWithEI());
+        }
+    }
+}
