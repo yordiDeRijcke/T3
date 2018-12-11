e471ab745bebab7718e2d5d45d5cd238792fedb8
yordiDeRijcke <yordi.derijcke@student.hogent.be>
2018-12-11 11:04:23 +0100

    Bill/Index.cshml: Fill the list of employees

---
+++ T3.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using T3.Core.Repositories;
+
+namespace T3.Web.Controllers
+{
+    [Authorize]
+    public class EmployeeController : Controller
+    {
+        #region Properties
+        private readonly IEmployeeRepository _employeeRepository;
+        #endregion
+
+        #region Constructor
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        #endregion
+
+        #region Methods
+        public IActionResult Index()
+        {
+            return View(_employeeRepository.GetAll());
+        }
+
+        public IActionResult GetEmployees()
+        {
+            return Json(_employeeRepository.GetAll());
+        }
+        #endregion
+    }
+}
---
+++ T3.Web/wwwroot/js/bill.index.js
@@ -0,0 +1,17 @@
+﻿"use strict";
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init(e) {
+    const employeeDOM = document.getElementById("employeeList");
+
+    $.getJSON("Employee/GetEmployees", function (data) {
+        $.each(data, function (i, employee) {
+            // Create new list-attribute for each employee
+            const newAttribute = document.createElement("a");
+            newAttribute.setAttribute("class", "list-group-item");
+            newAttribute.innerHTML = employee.name;
+            employeeDOM.appendChild(newAttribute);
+        });
+    });
+}
