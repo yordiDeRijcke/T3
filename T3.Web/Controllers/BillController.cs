38c60f26202ce099728c19f03c665befb2204e5b
yordiDeRijcke <yordi.derijcke@student.hogent.be>
2018-12-12 16:57:16 +0100

    Bill/Index.cshtml: Use BillIndexViewModel to supply Bills & Employees

--- T3.Web/Controllers/BillController.cs
+++ T3.Web/Controllers/BillController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using T3.Core.Repositories;
+using T3.Web.Models;
 
 namespace T3.Web.Controllers
 {
@@ -10,19 +11,27 @@ namespace T3.Web.Controllers
     {
         #region Properties
         private readonly IBillRepository _billRepository;
+        private readonly IEmployeeRepository _employeeRepository;
         #endregion
 
         #region Constructor
-        public BillController(IBillRepository billRepository)
+        public BillController(IBillRepository billRepository, IEmployeeRepository employeeRepository)
         {
             _billRepository = billRepository;
+            _employeeRepository = employeeRepository;
         }
         #endregion
 
         #region Methods
         public IActionResult Index()
         {
-            return View(_billRepository.GetAllWithEmployees());
+            BillIndexViewModel bivm = new BillIndexViewModel
+            {
+                Bills = _billRepository.GetAllWithEmployees(),
+                Employees = _employeeRepository.GetAll()
+            };
+
+            return View(bivm);
         }
         #endregion
     }
---
+++ T3.Web/Models/BillIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using T3.Core.Domain;
+
+namespace T3.Web.Models
+{
+    public class BillIndexViewModel
+    {
+        #region Properties
+        public List<Bill> Bills { get; set; }
+        public List<Employee> Employees { get; set; }
+        #endregion
+    }
+}
